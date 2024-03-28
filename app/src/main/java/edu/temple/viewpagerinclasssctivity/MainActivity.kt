e7ca33d9afb94d56b35bc3b8dcc1d9fbd7b74060
Daniel Lee <tur95049@temple.edu>
2024-03-28 11:41:53 -0400

    Create viewpager and use fragment state adapter to make pages

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -3,6 +3,7 @@ package edu.temple.viewpagerinclasssctivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 
@@ -12,6 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+
+        viewPager.adapter = object: FragmentStateAdapter (this) {
+            override fun getItemCount()= 10
+
+            override fun createFragment(position: Int) = TextFragment.newInstance((position + 1).toString())
+
+        }
     }
 
 }
